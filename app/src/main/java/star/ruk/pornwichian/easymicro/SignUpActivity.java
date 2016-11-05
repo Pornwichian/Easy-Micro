fb0fa0b8e25ef0c8f9b001a6b794fc08498e9a50
Pornwichian <Pornwichian@gmail.com>
2016-11-05 16:17:44 +0700

    test

--- app/src/main/java/star/ruk/pornwichian/easymicro/SignUpActivity.java
+++ app/src/main/java/star/ruk/pornwichian/easymicro/SignUpActivity.java
@@ -1,5 +1,9 @@
 package star.ruk.pornwichian.easymicro;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +19,7 @@ public class SignUpActivity extends AppCompatActivity {
     private ImageView imageView;
     private Button button;
     private String nameString,userString, passwordString;
+    private Uri uri;
 
 
 
@@ -56,24 +61,52 @@ public class SignUpActivity extends AppCompatActivity {
                             getResources().getString(R.string.massage_haveSpace));
                     myAlert.MyDialog();
 
-
-
                 }//If
-
-
-
-
-
 
             }//Onclick
         });
+            //Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent to other app
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"`โปรดเลือกโปรแกรมดู เพื่อเลือกรูป"),0);
 
 
+            }//OnClick
+        });
 
 
     }//Main method
 
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if((requestCode==0)&&(resultCode==RESULT_OK)){
+            //Result
+            Log.d("5NovV1", "Result OK");
+            uri = data.getData();
+
+            //Setup choose image
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                        .openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
 
 
+
+        }//if
+
+
+
+    }//On Activity
 }// Main Class
