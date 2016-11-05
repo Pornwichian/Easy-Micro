8d12c818ce6f289122c973000d147ae7a6043313
Pornwichian <Pornwichian@gmail.com>
2016-11-05 12:00:46 +0700

    vcs intent finish signup

--- app/src/main/java/star/ruk/pornwichian/easymicro/MainActivity.java
+++ app/src/main/java/star/ruk/pornwichian/easymicro/MainActivity.java
@@ -1,13 +1,49 @@
 package star.ruk.pornwichian.easymicro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private Button signInButton,signUpButton ;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+
+        ///Bind widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //SignUp controller
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+            }
+        });
+
+
+
+
+    } //Main method
+
+
+
+
+
+
+
+} // Main class
