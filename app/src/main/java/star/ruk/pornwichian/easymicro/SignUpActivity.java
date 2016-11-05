7961e3797e15297c3da3596681aa34fea0348632
Pornwichian <Pornwichian@gmail.com>
2016-11-05 14:41:16 +0700

    Check space

--- app/src/main/java/star/ruk/pornwichian/easymicro/SignUpActivity.java
+++ app/src/main/java/star/ruk/pornwichian/easymicro/SignUpActivity.java
@@ -2,12 +2,73 @@ package star.ruk.pornwichian.easymicro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText , passwordEditText;
+    private ImageView imageView;
+    private Button button;
+    private String nameString,userString, passwordString;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind widget
+
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+
+
+        //signUp controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get value frm edit text
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString .equals("")||
+                        userString.equals("")||
+                        passwordString.equals("") ) {
+
+                    //Have Space
+                    Log.d("5NovV1", "Have Space");
+
+
+                }//If
+
+
+
+
+
+
+            }//Onclick
+        });
+
+
+
+
+    }//Main method
+
+
+
+
+}// Main Class
